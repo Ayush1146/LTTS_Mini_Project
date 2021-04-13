4a720c74b0aa6c9dac6dcf19760ca5b12759cdf8
nikhiljose21 <johndoe@example.com>
2021-04-13 13:37:52 +0530

    modified main.c

--- main.c
+++ main.c
@@ -1,1 +1,26 @@
 #include<stdio.h>
+
+int main()
+{
+    printf("Enter the operation: \n1 = Resistance\n2 = capacitor\n3 = Inductor \n");
+    int choice;
+    scanf("%d",&choice);
+    printf("%d",choice);
+    switch (choice)
+    {
+    case 1:
+        resistance();
+        break;
+    case 2:
+        capacitance();
+        break;
+    case 3:
+        inductance();
+        break;
+    
+    default:
+        printf("Invalid choice\n");
+        break;
+    }
+
+}
